3708a5e9984e72a75da4a0645ddf0d09ba009fa6
VinuriGamage <89873897+VinuriGamage@users.noreply.github.com>
2022-09-06 05:51:19 +0530

    Update Q1.scala
    submitted practical 6

--- practical 6/Q1.scala
+++ practical 6/Q1.scala
@@ -2,24 +2,36 @@ object CeasarCipher extends App{
 
 	val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-	val shift = (scala.io.StdIn.readLine("Shift by: ").toInt + alphabet.size) % alphabet.size
+	val shift = (scala.io.StdIn.readLine("Shift number: ").toInt + alphabet.size) % alphabet.size
 
 	val inputText = scala.io.StdIn.readLine("Enter the code which you want to encrypt or decrypt: ")
 
-	val outputText = (shift:Int)=>inputText.map( (c: Char) => { 
+
+	val Encryption = (shift:Int)=>inputText.map( (c: Char) => { 
 
 		val x = alphabet.indexOf(c.toUpper)
 
 		if (x == -1) c
 
-		else alphabet((x + shift) % alphabet.size)
+		else alphabet((x + shift + alphabet.size) % alphabet.size)
 	});
 
-	println("Enter 1-to encrypt message");
-	println("Enter 2-to decrypt message");
+	
+	val Dycryption = (shift:Int)=>inputText.map( (c: Char) => { 
+
+		val x = alphabet.indexOf(c.toUpper)
+
+		if (x == -1) c
+
+		else alphabet((x - shift + alphabet.size) % alphabet.size)
+	});
+
+
+	println("Enter 1-to encrypt the code");
+	println("Enter 2-to decrypt the code");
 	val n=scala.io.StdIn.readInt()
 
-	if(n==1) println("Encrypted message: "+ outputText(shift))	
+	if(n==1) println("Encrypted message: "+ Encryption(shift))	
 
-	else println("Decrypted message: "+ outputText(-shift))
+	else println("Decrypted message: "+ Dycryption(shift))
 }
